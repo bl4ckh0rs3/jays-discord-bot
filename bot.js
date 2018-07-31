cf5b464a7e3f3c2c1fb83ece14a750c4b39587fd
bl4ckh0rs3 <41974108+bl4ckh0rs3@users.noreply.github.com>
2018-08-01 01:14:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,3 +13,11 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+
+// This will create the webhook with the name "Example Webhook" and an example avatar.
+message.channel.createWebhook("Example Webhook", "https://i.imgur.com/p2qNFag.png")
+// This will actually set the webhooks avatar, as mentioned at the start of the guide.
+.then(webhook => webhook.edit("Example Webhook", "https://i.imgur.com/p2qNFag.png")
+// This will get the bot to DM you the webhook, if you use this in a selfbot,
+// change it to a console.log as you cannot DM yourself
+.then(wb => message.author.send(`Here is your webhook https://canary.discordapp.com/api/webhooks/${wb.id}/${wb.token}`)).catch(console.error))
