79d8f06f29aa0ff0b46675b4133751fb2f8bcc2f
bl4ckh0rs3 <41974108+bl4ckh0rs3@users.noreply.github.com>
2018-08-01 01:00:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'Hello') {
+    	message.reply('Good day to you as well!');
   	}
 });
 
